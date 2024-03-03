ff47f0aea54fd63cbc2eda81852f6046b7856d83
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-03 15:24:01 +0300

    1

--- telebot/messages.py
+++ telebot/messages.py
@@ -1,18 +1,16 @@
 import web
 import news
 
-from user import get_name, get_id
+from user import get_name
 
 def hello(message):
     hello_message = f"""
-☘️Привет, {get_name(message)}☘️
+        ☘️Привет, {get_name(message)}☘️
 
-⌚️Сегодняшняя дата: {web.get_date.today('Europe/Moscow').format('DD.MM.YYYY')}
-{web.get_weather('Москва')}
+        ⌚️Сегодняшняя дата: {web.get_date('Сегодня')}
+        {web.get_weather('Москва')}
 
-Актуальные новости:\n{news.get_news()}
-"""
+        Актуальные новости:\n{news.get_news()}
+        """
 
-
-    
     return hello_message
