14d3128c485412f1cc6ebd324becafc6243c6264
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-03 21:42:00 +0300

    1

--- telebot/main.py
+++ telebot/main.py
@@ -1,28 +1,28 @@
 import telebot
-import pymysql
 import commands
 import database
-from config import host, user, password, db_name
 
 token = '6556635188:AAHgGkjUlc_lzhdQt_QgvEYMtClLyLdOBQE'
 bot = telebot.TeleBot(token)
+
+def get_db(message):
+    db = (f"id{message.from_user.id}")
+    return db
 
 try:
     database.connection()
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        db = (f"id{message.from_user.id}")
-        database.create_table(db)
+        database.create_table(get_db)
         commands.start(message)
 
     @bot.message_handler(content_types=['text'])
     def send_message(message):
         chatID = message.chat.id
-        db = (f"id{message.from_user.id}")
         match message.text:
             case "Посмотреть задачи":
-                bot.send_message(chatID, database.show_tasks(db))
+                bot.send_message(chatID, database.show_tasks(get_db))
 
 
         
