3aff47f943e9a345722e1292957ac454b873c437
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-03 15:46:55 +0300

    1

--- telebot/database.py
+++ telebot/database.py
@@ -1,6 +1,13 @@
 def create_table(connection, message):
     db = (f"id{message.from_user.id}")
 
+    def check_table():
+        with connection.cursor() as cursor:
+            query = f"SHOW TABLES LIKE '{db}'"
+            cursor.execute(query)
+            result = cursor.fetchone()
+            return result
+
     with connection.cursor() as cursor:
         if check_table(connection):
             print("существует")
@@ -8,12 +15,3 @@ def create_table(connection, message):
             cursor.execute(f"CREATE TABLE {str(db)} (id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), time VARCHAR(255), date VARCHAR(255), lon VARCHAR(255), lat VARCHAR(255))")
 
         connection.commit()
-            
-
-def check_table(connection, message):
-    db = (f"id{message.from_user.id}")
-    with connection.cursor() as cursor:
-        query = f"SHOW TABLES LIKE '{db}'"
-        cursor.execute(query)
-        result = cursor.fetchone()
-        return result
