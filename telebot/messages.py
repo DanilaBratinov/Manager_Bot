e93dbf2870027c477510ea28a0b41b9bdf298814
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-04 17:41:17 +0300

    Commit

--- telebot/messages.py
+++ telebot/messages.py
@@ -1,12 +1,15 @@
 import web
+import database
+from commands import bot
 import news
 from user import get_name
 
 def hello(message):
     hello_message = f"""
 ☘️Привет, {get_name(message)}☘️
-******************************
-
+***********************************
+{database.show_tasks(bot, message)}
+***********************************
 ⌚️Сегодняшняя дата: {web.get_date('Сегодня').format('DD.MM.YYYY')}
 {web.get_weather('Москва')}
 
@@ -14,5 +17,3 @@ def hello(message):
 """
 
     return hello_message
-
-# {database.show_tasks(database.connection, database.get_db)}
