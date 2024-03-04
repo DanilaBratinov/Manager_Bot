df64c2cce8809befadccc45ae07b4e7ecce0fd03
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-04 16:31:45 +0300

    1

--- telebot/database.py
+++ telebot/database.py
@@ -32,6 +32,8 @@ def create_table(db):
 def add_tasks(message):
     bot.send_message(message.chat.id, 'Введите название задачи:')
     bot.register_next_step_handler(message, add_task_name(message))
+    print(message.text)
+    print("Тест")
 
 task = ["", "", ""] 
 
