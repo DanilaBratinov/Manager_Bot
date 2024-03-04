d613ebdfffbab03f4b0ca2394241bbe50909c41f
DanilaBratinov <116026697+DanilaBratinov@users.noreply.github.com>
2024-03-04 15:59:45 +0300

    1

--- telebot/database.py
+++ telebot/database.py
@@ -5,6 +5,7 @@ from telebot import types
 from config import host, user, password, db_name
 
 bot = commands.bot
+
 # Connect to MySQL-Server
 connection = pymysql.connect(
         host = host,
@@ -33,43 +34,43 @@ def add_tasks(bot, message):
     bot.send_message(message.chat.id, 'Введите название задачи:')
     bot.register_next_step_handler(message, add_task_name(bot, message))
 
-    task = ["", "", ""] 
+task = ["", "", ""] 
 
     #Название задачи
-    def add_task_name(bot, message):
-        task[0] = message.text
-        bot.send_message(message.chat.id, "Введите время:")
-        bot.register_next_step_handler(message, add_task_time(bot, message))
+def add_task_name(bot, message):
+    task[0] = message.text
+    bot.send_message(message.chat.id, "Введите время:")
+    bot.register_next_step_handler(message, add_task_time(bot, message))
 
     #Время
-    def add_task_time(bot, message):
-        task[1] = (message.text)
+def add_task_time(bot, message):
+    task[1] = (message.text)
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        item1 = types.KeyboardButton('Сегодня')
-        item2 = types.KeyboardButton('Завтра')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('Сегодня')
+    item2 = types.KeyboardButton('Завтра')
 
-        markup.add(item1, item2)
+    markup.add(item1, item2)
 
-        bot.send_message(message.chat.id, "Выберите дату:", reply_markup = markup)
-        bot.register_next_step_handler(message, add_task_date(bot, message))
+    bot.send_message(message.chat.id, "Выберите дату:", reply_markup = markup)
+    bot.register_next_step_handler(message, add_task_date(bot, message))
         
     #Дата
-    def add_task_date(bot, message):
-        task[2] = web.get_date(message.text)
+def add_task_date(bot, message):
+    task[2] = web.get_date(message.text)
 
-        with connection.cursor() as cursor:
-            cursor.execute(f"INSERT INTO {str(get_db)} (time, name, date) VALUES ('{task[1]}', '{task[0]}', '{task[2]}');")
-            connection.commit()
+    with connection.cursor() as cursor:
+        cursor.execute(f"INSERT INTO {str(get_db)} (time, name, date) VALUES ('{task[1]}', '{task[0]}', '{task[2]}');")
+        connection.commit()
 
-        markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-        item1 = types.KeyboardButton('Добавить задачу')
-        item2 = types.KeyboardButton('Посмотреть задачи')
-        item3 = types.KeyboardButton('Очистить список')
-        item4 = types.KeyboardButton('Главная')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('Добавить задачу')
+    item2 = types.KeyboardButton('Посмотреть задачи')
+    item3 = types.KeyboardButton('Очистить список')
+    item4 = types.KeyboardButton('Главная')
 
-        markup.add(item1, item2, item3, item4)
-        bot.send_message(message.chat.id, f"Добавлена задача: *{task[0]}*", parse_mode="Markdown", reply_markup = markup) 
+    markup.add(item1, item2, item3, item4)
+    bot.send_message(message.chat.id, f"Добавлена задача: *{task[0]}*", parse_mode="Markdown", reply_markup = markup) 
 
 # Show today tasks
 def show_tasks(bot, message):
